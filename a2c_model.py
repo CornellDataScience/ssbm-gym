de6ee42fa535ba9433e6018063393f92fcbd58eb
xforeverjlx <johannchrislee@gmail.com>
2022-04-19 20:13:55 -0400

    added layers to a2c model

--- a2c_model.py
+++ a2c_model.py
@@ -6,10 +6,17 @@ class Actor(nn.Module):
     def __init__(self, obs_dim, action_dim, hidden_dim = 256):
         """initializes embedding, value network (ResNet, state -> value), and policy network (state -> action) """
         super(Actor, self).__init__()
-        # self.fc = nn.Linear(obs_dim, hidden_dim)
         self.fc = nn.Linear(obs_dim * 2, hidden_dim)
         self.value = ResNet(hidden_dim, 1, 2, output_dim=1)
-        self.policy = nn.Linear(hidden_dim, action_dim)
+        self.policy = nn.Sequential(nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(True),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(True),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(True),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(True),
+            nn.Linear(hidden_dim, action_dim))
 
     def forward(self, x):
         """ given obs, returns action_probability, estimated_value. Actions are from x |> embedding |> value_network |> policy_network. Estimated_value from x |> embedding |> value_network. """
