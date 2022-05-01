bug fixes, backprop now working

StateNet.py
@@ -10,7 +10,7 @@ class StateNet(nn.Module):
         super(StateNet, self).__init__()
         self.fc1 = nn.Linear(obs_dim * 2 + 1, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc3 = nn.Linear(hidden_dim, obs_dim)
+        self.fc3 = nn.Linear(hidden_dim, obs_dim * 2)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))

train_state.py
@@ -199,12 +199,14 @@ def update_state_network(params, state_net, optimizer_state, action_buffer, stat
         input = torch.cat((act, obs), dim = 1)
 
         pred = state_net(input)
-
-        loss = MSEloss(pred, state_buffer[0])
+        print(pred.shape)
+        print(state_buffer[0].shape)
+        criterion = nn.MSELoss()
+        loss = criterion(pred, state_buffer[0])
         loss.backward()
 
-        optimizer.step()
-        optimizer.zero_grad()
+        optimizer_state.step()
+        optimizer_state.zero_grad()
 
         action_buffer = action_buffer[-1]
         state_buffer = state_buffer[-1]